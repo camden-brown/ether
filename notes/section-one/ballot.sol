437122695975c8316ae1a386dd4768da6e0171cb
Camden Brown <camdenebrown@gmail.com>
2022-05-04 08:25:08 -0500

    Review need for getter

--- notes/section-one/ballot.sol
+++ notes/section-one/ballot.sol
@@ -7,18 +7,12 @@ pragma solidity ^0.4.17;
 contract Inbox {
     string public message;
 
+/* Constructor Function */
     function Inbox(string initialMessage) public {
         message = initialMessage;
     }
 
     function setMessage(string newMessage) public {
         message = newMessage;
-    }
-
-/*
- Function type - "view" is basically a getter 
-*/
-    function getMessage() public view returns (string) {
-        return message;
     }
 }
