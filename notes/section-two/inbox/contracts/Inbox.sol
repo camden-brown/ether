117ec90e76e8d5d1946fbaaf0e6e382fc694045c
Camden Brown <camdenebrown@gmail.com>
2022-05-16 10:09:52 -0500

    * create solidity project

---
+++ notes/section-two/inbox/contracts/Inbox.sol
@@ -0,0 +1,14 @@
+// Version of Solidity
+pragma solidity ^0.4.17;
+
+contract Inbox {
+  string public message;
+
+  function Inbox(string initialMessage) public {
+    message = initialMessage;
+  }
+
+  function setMessage(string newMessage) public {
+    message = newMessage;
+  }
+}
